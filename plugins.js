8f183e48b160cd321bf1dc30c4c7bbaac954f330
Jesús Moreno <jmm12392@gmail.com>
2023-04-18 22:03:47 -0400

    Fix linter errors

--- plugins.js
+++ plugins.js
@@ -1,6 +1,6 @@
 const hamburger = document.querySelector('.ham-icon');
 const navMenu = document.querySelector('.nav-menu');
-const nav=document.querySelectorAll('.nav-item');
+const nav = document.querySelectorAll('.nav-item');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
@@ -9,6 +9,6 @@ hamburger.addEventListener('click', () => {
 nav.forEach((e) => {
   e.addEventListener('click', () => {
     hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
   });
 });
