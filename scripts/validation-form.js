52655b3cf499ba6f08ce9319bc97106ce9086884
Jesús Moreno <jmm12392@gmail.com>
2023-04-21 17:35:31 -0400

    Fix linters errors

--- scripts/validation-form.js
+++ scripts/validation-form.js
@@ -2,7 +2,7 @@ document.getElementById('contact-form').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
   if (email !== email.toLowerCase()) {
     event.preventDefault();
-    document.querySelector('.error').innerHTML = 'Invalid email address, only use lowercase letters'
+    document.querySelector('.error').innerHTML = 'Invalid email address, only use lowercase letters';
     event.target.reset();
   }
 });
