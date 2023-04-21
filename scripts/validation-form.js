9d03deaf5604a3ac56fc20712ed7058d24a76806
Betel Andarge <91183694+beteland123@users.noreply.github.com>
2023-04-21 23:11:56 +0300

    Create the JavaScript validation form

--- scripts/validation-form.js
+++ scripts/validation-form.js
@@ -1,1 +1,8 @@
-
+document.getElementById('contact-form').addEventListener('submit', (event) => {
+  const email = document.getElementById('email').value;
+  if (email !== email.toLowerCase()) {
+    event.preventDefault();
+    alert('Invalid email address!');
+    event.target.reset();
+  }
+});
