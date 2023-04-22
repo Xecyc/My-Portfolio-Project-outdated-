ff586500167a16cc107e65284de79183ec2bd18f
Betel Andarge <91183694+beteland123@users.noreply.github.com>
2023-04-22 20:03:31 +0300

    ADD the object of an array

---
+++ scripts/pop-up.js
@@ -0,0 +1,10 @@
+const works = [
+  {
+    title: 'Multi-Post-Stories',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    featured_image: './img/placeholders/snapshoot-mobile.png',
+    lang_list: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+    source: 'https://github.com/Xecyc/My-Portfolio-Project',
+    live_demo: 'https://xecyc.github.io/My-Portfolio-Project/',
+  },
+];
